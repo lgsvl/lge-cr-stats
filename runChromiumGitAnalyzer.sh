1b5b7d7bea102e0443dc29b87192e0f522ca100e
Gyuyoung Kim <gyuyoung.kim@lge.com>
2017-11-23 15:11:59 +0900

    Update runChromiumGitAnalyzer.sh

--- runChromiumGitAnalyzer.sh
+++ runChromiumGitAnalyzer.sh
@@ -3,7 +3,7 @@
 # Define pathes for this tool and Chromium source.
 GIT_COUNTER_PATH=$HOME/github/Chromium-stats
 CHROMIUM_PATH=$HOME/chromium-stats/chromium
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/gyuyoung/github/Chromium-stats:$PATH
+PATH=$GIT_COUNTER_PATH:$PATH
 
 # Update Chromium source code.
 cd $CHROMIUM_PATH
@@ -12,13 +12,13 @@ timestamp=$(date +"%T")
 echo "[$timestamp] Finish to update Chromium."
 
 # Start to analyze LGE commit counts.
+cd $GIT_COUNTER_PATH
 now="$(date +'%Y-%m-%d')"
 timestamp=$(date +"%T")
 echo "[$timestamp] Starting checking foo@lge.com commits from 2017-01-01 to $now, please wait..."
 gitinspector.py --format=html --since="2017-01-01" --until="$now" -T -x "email:^(?!([a-zA-Z0-9._-]+@lge.com))" $CHROMIUM_PATH > $GIT_COUNTER_PATH/index.html
 
 # Upload the result to github.
-cd $GIT_COUNTER_PATH
 git add index.html
 git commit -m "Update index.html by bot"
 git push origin master:master
