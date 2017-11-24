4d854aaba985ed5605c982be81fa2ef11279f3cf
Gyuyoung Kim <gyuyoung.kim@lge.com>
2017-11-25 00:02:05 +0900

    Update runChromiumGitAnalyzer.sh

--- runChromiumGitAnalyzer.sh
+++ runChromiumGitAnalyzer.sh
@@ -3,14 +3,13 @@
 # Define pathes for this tool and Chromium source.
 GIT_COUNTER_PATH=$HOME/github/Chromium-stats
 CHROMIUM_PATH=$HOME/chromium-stats/chromium
-LOG_PATH=$HOME/chromium-stats/log
 GIT_INSPECTOR_PATH=$HOME/github/gitinspector
 export PATH=$GIT_INSPECTOR_PATH:$PATH
 
 while :
 do
     # Update Chromium source code.
-    cd $CHROMIUM_PATH
+    cd "$CHROMIUM_PATH"
     git pull origin master:master
     timestamp=$(date +"%T")
     echo "[$timestamp] Finish to update Chromium."
@@ -18,11 +17,11 @@ do
     # Start to analyze LGE commit counts.
     now="$(date +'%Y-%m-%d')"
     timestamp=$(date +"%T")
-    echo "[$timestamp] Starting checking foo@lge.com commits from 2017-09-01 to $now, please wait..."
-    gitinspector.py --format=html --since="2017-09-01" --until="$now" -T -x "email:^(?!([a-zA-Z0-9._-]+@lge.com))" $CHROMIUM_PATH > $GIT_COUNTER_PATH/index.html
+    echo "[$timestamp] Starting checking foo@lge.com commits from 2017-01-01 to $now, please wait..."
+    gitinspector.py --format=html --since="2017-01-01" --until="$now" -T -x "email:^(?!([a-zA-Z0-9._-]+@lge.com))" "$CHROMIUM_PATH" > "$GIT_COUNTER_PATH"/index.html
 
     # Upload the result to github.
-    cd $GIT_COUNTER_PATH
+    cd "$GIT_COUNTER_PATH"
     git add index.html
     git commit -m "Update index.html by bot"
     git push origin master:master
